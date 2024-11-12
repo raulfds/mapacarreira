1ee315a76d7be286c8a5a0bf0592dc638f032cb9
raulfds <96803581+raulfds@users.noreply.github.com>
2024-11-12 12:04:24 -0300

    v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,18 +16,7 @@ export default {
           800: '#075985',
           900: '#0c4a6e',
         },
-        secondary: {
-          50: '#fdf4ff',
-          100: '#fae8ff',
-          200: '#f5d0fe',
-          300: '#f0abfc',
-          400: '#e879f9',
-          500: '#d946ef',
-          600: '#c026d3',
-          700: '#a21caf',
-          800: '#86198f',
-          900: '#701a75',
-        },
+       
       },
       boxShadow: {
         'inner-lg': 'inset 0 2px 4px 0 rgb(0 0 0 / 0.05)',
